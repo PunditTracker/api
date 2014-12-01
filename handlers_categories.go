c361807bfecc6948bdcf61a1ac783eafe323b656
Ben Levy <bjlgds@gmail.com>
2014-12-01 10:20:57 -0500

    category handlers now spit out json

--- handlers_categories.go
+++ handlers_categories.go
@@ -1,19 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
+	"strconv"
 )
 
 func GetCategoriesHandler(w http.ResponseWriter, r *http.Request) {
 	db, _ := getDB()
 	categories := GetCategories(db)
-	fmt.Fprintln(w, categories)
+	j, _ := json.Marshal(categories)
+	fmt.Fprintln(w, string(j))
 }
 
 func GetSubcategoriesHandler(w http.ResponseWriter, r *http.Request) {
 	db, _ := getDB()
-	categoryId := int64(1)
-	subcats := GetSubcategoriesWithCategoryId(db, categoryId)
-	fmt.Fprintln(w, subcats)
+	vars := mux.Vars(r)
+	categoryId, _ := strconv.Atoi(vars["id"])
+	subcats := GetSubcategoriesWithCategoryId(db, int64(categoryId))
+	j, _ := json.Marshal(subcats)
+	fmt.Fprintln(w, string(j))
+}
+
+func GetSubcategoriesWithNameHandler(w http.ResponseWriter, r *http.Request) {
+	db, _ := getDB()
+	vars := mux.Vars(r)
+	categoryName := vars["name"]
+	subcats := GetSubcategoriesWithCategoryName(db, categoryName)
+	j, _ := json.Marshal(subcats)
+	fmt.Fprintln(w, string(j))
 }
