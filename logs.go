935cccaa522d7cb75579a5972011b51f76891aa2
Blevz <Bjlgds@gmail.com>
2015-01-26 20:04:53 -0500

    url routing now saved in url_log.txt

--- logs.go
+++ logs.go
@@ -15,7 +15,7 @@ func init() {
 		panic("no db log")
 	}
 	db_logger = log.New(db_f, "db: ", log.LstdFlags|log.Lshortfile)
-	request_f, err = os.OpenFile("log.txt", os.O_RDWR|os.O_APPEND, 0660)
+	request_f, err = os.OpenFile("url_log.txt", os.O_RDWR|os.O_APPEND, 0660)
 	if err != nil {
 		panic("no url routing log")
 	}
