d65bf78a1f831ee0b61ec40bf5ade60675256264
EC2 Default User <ec2-user@ip-172-31-25-19.us-west-2.compute.internal>
2015-02-04 00:51:29 +0000

    should be aws

--- reload.sh
+++ reload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 go build
-export SERV=local
+export SERV=aws
 export AWS_ACCESS_KEY_ID="AKIAJUAA7V5XHKH2A75Q"
 export AWS_SECRET_ACCESS_KEY="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"
 kill $(pgrep webBackend)
