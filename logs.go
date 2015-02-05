730d4a95f680c26f2492783a94bf5ff95278d358
Blevz <Bjlgds@gmail.com>
2015-02-05 15:54:56 -0500

    fix weird crashing when logs are deleted

--- logs.go
+++ logs.go
@@ -10,12 +10,13 @@ var (
 )
 
 func init() {
-	db_f, err := os.OpenFile("db_log.txt", os.O_RDWR|os.O_APPEND, 0660)
+
+	db_f, err := os.OpenFile("db_log.txt", os.O_RDWR|os.O_APPEND|O_CREATE, 0660)
 	if err != nil {
 		panic("no db log")
 	}
 	db_logger = log.New(db_f, "db: ", log.LstdFlags|log.Lshortfile)
-	request_f, err = os.OpenFile("url_log.txt", os.O_RDWR|os.O_APPEND, 0660)
+	request_f, err = os.OpenFile("url_log.txt", os.O_RDWR|os.O_APPEND|O_CREATE, 0660)
 	if err != nil {
 		panic("no url routing log")
 	}
--- reload.sh
+++ reload.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 go build
-export SERV=local
+export SERV=aws
 export AWS_ACCESS_KEY_ID="AKIAJUAA7V5XHKH2A75Q"
 export AWS_SECRET_ACCESS_KEY="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"
 export AWS_SECRET_KEY="r/mF80Scdrae19g25Hu7SyjmIdsS8GasD7B0mKr3"
-kill "$(pgrep webBackend)"
+kill $(pgrep webBackend)
 ./webBackend 2>>err.txt 1>>out.txt &
 echo "Server is launched with PID:" 
-echo "$!"
+echo $!
 disown
