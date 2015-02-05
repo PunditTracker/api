53b98a2a9c6b6378b3f01034bfa21e48cda36909
Blevz <Bjlgds@gmail.com>
2015-02-05 14:21:40 -0500

    cleaned up reload.sh with shellcheck

--- reload.sh
+++ reload.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 go build
-export SERV=aws
+export SERV=local
 export AWS_ACCESS_KEY_ID="AKIAJUAA7V5XHKH2A75Q"
 export AWS_SECRET_ACCESS_KEY="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"
-kill $(pgrep webBackend)
+export AWS_SECRET_KEY="r/mF80Scdrae19g25Hu7SyjmIdsS8GasD7B0mKr3"
+kill "$(pgrep webBackend)"
 ./webBackend 2>>err.txt 1>>out.txt &
 echo "Server is launched with PID:" 
-echo $!
+echo "$!"
 disown
