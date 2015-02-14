655a69f1f3de89dfe547ffcd08579fb3f6943ed5
Blevz <Bjlgds@gmail.com>
2015-02-14 16:27:19 -0500

    fix reload script

--- reload.sh
+++ reload.sh
@@ -5,7 +5,7 @@ export AWS_ACCESS_KEY_ID="AKIAJUAA7V5XHKH2A75Q"
 export AWS_SECRET_ACCESS_KEY="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"
 export AWS_SECRET_KEY="r/mF80Scdrae19g25Hu7SyjmIdsS8GasD7B0mKr3"
 kill "$(pgrep webBackend)"
-./webBackend
+./webBackend &
 echo "Server is launched with PID:" 
 echo "$!"
 disown
