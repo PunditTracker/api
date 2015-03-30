1fea788e5ef3ca91c40726e7aeb70aaf04c3326d
blevz <bjlgds@gmail.com>
2015-03-30 18:10:47 -0400

    change reload script to reflect api name

--- reload.sh
+++ reload.sh
@@ -4,8 +4,8 @@ export SERV=local
 export AWS_ACCESS_KEY_ID="AKIAJUAA7V5XHKH2A75Q"
 export AWS_SECRET_ACCESS_KEY="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"
 export AWS_SECRET_KEY="r/mF80Scdrae19g25Hu7SyjmIdsS8GasD7B0mKr3"
-kill "$(pgrep webBackend)"
-./webBackend &
+kill "$(pgrep api)"
+./api &
 echo "Server is launched with PID:" 
 echo "$!"
 disown
